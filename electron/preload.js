d9dff102bbc3f945d21261ced167ccf90d75d804
unknown <dhruvdumra@gmail.com>
2026-06-22 19:13:06 +0530

    feat(electron): main process and preload bridge

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('deck', {
-  openFile: (opts) => ipcRenderer.invoke('file:open', opts ?? {}),
-  saveFile: (defaultName, content) =>
-    ipcRenderer.invoke('file:save', { defaultName, content }),
-  platform: process.platform,
-})
